b7f41c2a54cb056ab9a945f2408d93c1222efa1e
MohdFaisalBidda <biddafaisal@gmail.com>
2024-04-03 02:11:00 +0530

    Feat: fetched data and rendered on home page.

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="p-6 text-4xl font-mono bg-[#34AC9C] rounded-b-2xl">
+      Movie Awards 2021 🎬
+    </div>
+  );
+}
+
+export default Header;
